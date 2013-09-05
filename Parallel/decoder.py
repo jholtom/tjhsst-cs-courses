ed9ef0a1b0ab4b00a77e918156f3d610be2619ee
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-09-04 21:26:11 -0400

    solved words

--- Parallel/decoder.py
+++ Parallel/decoder.py
@@ -1,16 +1,20 @@
-#
 # Jacob Holtom - Sept 3, 2013
-#
-#NODE CLASS FOR TREE
-01
-class Node:
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
+def decode(msg,encoding):
+    i = msg[:1]
+    k = 1
+    if msg == "":
+        return;
+    while(("1" + i) not in encoding.keys()):
+        i += msg[k:k+1]
+        k += 1
+    print encoding["1" + i],
+    decode(msg[len(i):],encoding)
 
-#Open the file and read to an array
-file = open("decode.txt").read().split();
-#Print it
-print file;
+file = open("decode.txt").read().split("\r\n");
+file.pop()
+message = file.pop()
+matrix = {}
 for i in file:
+    matrix[str(int("1"+i[1:]))] = i[:1]
+
+decode(message,matrix)
