2e830c443198a8641ec9126474d30d8494a5ae5a
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-09-10 21:32:18 -0400

    started encoder

---
+++ Parallel/encode.c
@@ -0,0 +1,17 @@
+//Jacob Holtom - Sept 10, 2013
+typedef struct TreeNode {
+    char key;
+    int freq;
+    struct TreeNode* left;
+    struct TreeNode* right;
+} node;
+void main(void)
+{
+char* msg = "HELLOWORLD";
+node* arr[7];
+int num = 7;
+for(int i; i<7;i++)
+{
+
+}
+}
