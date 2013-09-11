80cf7525033f1d290fe0ecb136b3e5cc6c66949e
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-09-11 14:49:33 -0400

    yay!

--- Parallel/encode.c
+++ Parallel/encode.c
@@ -1,4 +1,7 @@
 //Jacob Holtom - Sept 10, 2013
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 typedef struct TreeNode {
     char key;
     int freq;
@@ -9,10 +12,22 @@ void main(void)
 {
 char* msg = "HELLOWORLD";
 node* arr[7];
+int c = 0, count[26] = {0};
+while( msg[c] != '\0')
+{
+   if( msg[c] >= 'a' && msg[c] <= 'z' )
+       count[msg[c]-'a']++;
+
+   c++;
+}
+for(c = 0; c < 26; c++)
+{
+  if( count[c] != 0 )
+      printf("%c occured %d times.\n",c+'a',count[c]);
+}
 int num = 7;
 int i;
 for(i = 0; i<7;i++)
 {
-
 }
 }
