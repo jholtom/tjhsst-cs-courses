23d840e5a1065d46c8268426f51a7fab6dcd1276
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-09-16 16:33:53 -0400

    yay. much success

--- Parallel/encode.c
+++ Parallel/encode.c
@@ -2,26 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int[] bubble_sort(int arr[],len)
-{
-for()
-{
-for{
-if(){
-
-}
-}
-}
-
-}
-
 typedef struct TreeNode {
     char key;
     int freq;
     struct TreeNode* left;
     struct TreeNode* right;
 } node;
+void bubble_sort(node[], int);
+void bubble_sort(node targ[], int n)
+{
+int c,d;
+node t;
+for(c = 0; c < (n-1); c++)
+{
+for(d = 0; d < n - c -1; d++){
+if(targ[d].freq < targ[d+1].freq){
+t = targ[d];
+targ[d] = targ[d+1];
+targ[d+1] = t;
+}
+}
+}
+}
 void main(void)
 {
 char* msg = "HELLOWORLD";
@@ -49,5 +51,11 @@ for(c = 0; c < 26; c++)
       num += 1;
       }
   }
+}
+bubble_sort(arr,7);
+int i = 0;
+for(i = 0; i < 7; i++)
+{
+printf("Character: %c Position: %d Frequency: %d \n",arr[i].key,i,arr[i].freq); 
 }
 }
