82b09109420fffa8cf319c0c218dcdc861006b1e
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-09-16 16:15:35 -0400

    added array former

--- Parallel/encode.c
+++ Parallel/encode.c
@@ -11,7 +11,7 @@ typedef struct TreeNode {
 void main(void)
 {
 char* msg = "HELLOWORLD";
-node* arr[7];
+node arr[7];
 int c = 0;
 int count[26] = {0};
 while( msg[c] != '\0')
@@ -22,14 +22,18 @@ while( msg[c] != '\0')
    }
    c++;
 }
+int num = 0;
 for(c = 0; c < 26; c++)
 {
-  if( count[c] != 0 )
+  if( count[c] != 0 ){
       printf("%c occured %d times.\n",c+'A',count[c]);
-}
-int num = 7;
-int i;
-for(i = 0; i<7;i++)
-{
+      if(num <  7){
+      node x;
+      arr[num] = x;
+      arr[num].key = c+'A';
+      arr[num].freq = count[c];
+      num += 1;
+      }
+  }
 }
 }
