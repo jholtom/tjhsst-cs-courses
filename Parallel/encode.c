a18c4b820eb5e626f9320f31a84d4195f5a55f14
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-09-16 17:49:14 -0400

    pointers and stuff

--- Parallel/encode.c
+++ Parallel/encode.c
@@ -8,54 +8,68 @@ typedef struct TreeNode {
     struct TreeNode* left;
     struct TreeNode* right;
 } node;
-void bubble_sort(node[], int);
-void bubble_sort(node targ[], int n)
+void bubble_sort(node*[], int);
+void bubble_sort(node* targ[], int n)
 {
-int c,d;
-node t;
-for(c = 0; c < (n-1); c++)
-{
-for(d = 0; d < n - c -1; d++){
-if(targ[d].freq < targ[d+1].freq){
-t = targ[d];
-targ[d] = targ[d+1];
-targ[d+1] = t;
-}
-}
-}
+    int c,d;
+    node* t;
+    for(c = 0; c < (n-1); c++)
+    {
+        for(d = 0; d < n - c -1; d++){
+            if(targ[d]->freq < targ[d+1]->freq){
+                t = targ[d];
+                targ[d] = targ[d+1];
+                targ[d+1] = t;
+            }
+        }
+    }
 }
 void main(void)
 {
-char* msg = "HELLOWORLD";
-node arr[7];
-int c = 0;
-int count[26] = {0};
-while( msg[c] != '\0')
-{
-   if( msg[c] >= 'A' && msg[c] <= 'Z' )
+    char* msg = "HELLOWORLD";
+    node* arr[7];
+    int c = 0;
+    int count[26] = {0};
+    while( msg[c] != '\0')
+    {
+        if( msg[c] >= 'A' && msg[c] <= 'Z' )
+        {
+            count[msg[c]-'A']++;
+        }
+        c++;
+    }
+    int num = 0;
+    for(c = 0; c < 26; c++)
+    {
+        if( count[c] != 0 ){
+            printf("%c occured %d times.\n",c+'A',count[c]);
+            if(num <  7){
+                node* x;
+                arr[num] = x;
+                arr[num]->key = c+'A';
+                arr[num]->freq = count[c];
+                num += 1;
+            }
+        }
+    }
+    bubble_sort(arr,7);
+    int i = 0;
+    for(i = 0; i < 7; i++)
+    {
+        printf("Character: %c Position: %d Frequency: %d \n",arr[i].key,i,arr[i].freq); 
+    }
+   node* root;
+   root->key = '*';
+   root->freq = 10; 
+   int m = 6;
+   while(m >= 0)
    {
-       count[msg[c]-'A']++;
+       struct TreeNode* replace;
+       replace->key = '*';
+       replace->freq = (arr[m].freq + arr[m-1].freq);
+       replace->left = arr[m-1];
+       replace->right = arr[m];
+       arr[m] = NULL;
+       arr[m-1] = replace;
    }
-   c++;
-}
-int num = 0;
-for(c = 0; c < 26; c++)
-{
-  if( count[c] != 0 ){
-      printf("%c occured %d times.\n",c+'A',count[c]);
-      if(num <  7){
-      node x;
-      arr[num] = x;
-      arr[num].key = c+'A';
-      arr[num].freq = count[c];
-      num += 1;
-      }
-  }
-}
-bubble_sort(arr,7);
-int i = 0;
-for(i = 0; i < 7; i++)
-{
-printf("Character: %c Position: %d Frequency: %d \n",arr[i].key,i,arr[i].freq); 
-}
 }
