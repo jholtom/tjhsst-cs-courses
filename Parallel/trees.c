608f5b78830a22fa32e7c15572a88ae551a6feeb
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-10-02 09:31:25 -0400

    tree maker

--- Parallel/trees.c
+++ Parallel/trees.c
@@ -3,23 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ARRX 80
-#define ARRY 80
+#define ARRX 8
+#define ARRY 8
+#define PROB 70
 
-void draw_array(char[][]);
-void draw_array(char arr[][]){
-int x = 0;
-int y = 0;
+int main(void){
+    char field[ARRX][ARRY];
+    int k;
+    int m;
+    srand(time(NULL));
+    for(k = 0; k < ARRX; k++){
+        for(m = 0; m < ARRY; m++)
+        {
+            if((rand()%100 + 1) < PROB)
+            {
+                field[k][m] = 'T';
+            }
+            else{
+                field[k][m] = ' ';
+            }
+        }
+    }
+    int x,y;
 for(x = 0; x < ARRY; x++)
-{
-for(y = 0; y < ARRX; y++)
-{
-printf("%s",arr[x][y]);
-}
-printf("\n");
-}
-} 
-
-void main(void){
-
+    {
+        for(y = 0; y < ARRX; y++)
+        {
+            printf("%c",field[x][y]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
