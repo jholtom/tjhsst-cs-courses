cabc17a14a07af6b293574c28b27e0387b31f4d2
Jacob Holtom <webmaster@legobrains.com>
2013-10-03 03:10:02 -0400

    removed execs

--- AI/words.py
+++ AI/words.py
@@ -14,7 +14,7 @@ def wordsearch(word_dict, source, target):
     word_q = deque([[source]])
     visited = set([source])
     while word_q:
-        neighbors = word_q.popleft()
+        neighbors = word_q.pop() # POPLEFT FOR BFS / SET TO DFS NOW
         if neighbors[-1] == target:
             return neighbors
         adjacency = adjacent_to(word_dict, neighbors[-1])
