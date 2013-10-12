7ca8c86b6515096b2e4110477712ab683b1a4bd7
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-10-12 10:11:12 -0400

    magic

--- AI/edgesearch/edges.py
+++ AI/edgesearch/edges.py
@@ -35,8 +35,14 @@ class BFS:
 nodefile = open('nodes.txt','r')
 edgefile = open('edges.txt','r')
 nodelist = list(nodefile)
+edgelist = list(edgefile)
 nodedict = {}
+edgedict = {}
 for i in range(len(nodelist)):
     nodedict[nodelist[i].split(' ')[0]] = [nodelist[i].split(' ')[1],nodelist[i].split(' ')[2].split('\n')[0]]
 
+for i in range(len(edgelist)):
+    edgedict[edgelist[i].split(' ')[0]] = [edgelist[i].split(' ')[1],edgelist[i].split(' ')[2],edgelist[i].split(' ')[3].split('\n')[0]]
+
 print nodedict
+print edgedict
