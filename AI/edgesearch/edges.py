4e77b611f6cfa060e10fded459e2bd809c69e6c5
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-10-15 19:03:23 -0400

    fixes

--- AI/edgesearch/edges.py
+++ AI/edgesearch/edges.py
@@ -17,30 +17,29 @@ class Graph:
  
  
 def dijkstra(graph, initial):
-  visited = {initial: 0}
-  path = {}
-  nodes = set(graph.nodes)
-  while nodes: 
-    min_node = None
-    for node in nodes:
-      if node in visited:
-        if min_node is None:
-          min_node = node
-        elif visited[node] < visited[min_node]:
-          min_node = node
+    visited = {initial: 0}
+    path = {}
+    nodes = set(graph.nodes)
+    while nodes: 
+      min_node = None
+      for node in nodes:
+        if node in visited:
+          if min_node is None:
+            min_node = node
+          elif visited[node] < visited[min_node]:
+            min_node = node
  
-    if min_node is None:
-      break
+      if min_node is None:
+        break
+      nodes.remove(min_node)
+      current_weight = visited[min_node]
+      for edge in graph.edges[min_node]:
+          weight = float(current_weight) + float(graph.distances[(min_node, edge)])
+          if edge not in visited or weight < visited[edge]:
+              visited[edge] = weight
+              path[edge] = min_node
  
-    nodes.remove(min_node)
-    current_weight = visited[min_node]
-    for edge in graph.edges[min_node]:
-      weight = float(current_weight) + float(graph.distances[(min_node, edge)])
-      if edge not in visited or weight < visited[edge]:
-        visited[edge] = weight
-        path[edge] = min_node
- 
-  return visited, path
+    return visited, path
 
 nodefile = open('nodes.txt','r')
 edgefile = open('edges.txt','r')
@@ -65,4 +64,4 @@ for i in nodedict.keys():
 for i in edgedict:
     graph.add_edge(edgedict[i][0],edgedict[i][1],edgedict[i][2])
 
-print dijkstra(graph,nodelist[0].split(' ')[0]) 
+print dijkstra(graph,"0600209")
