9ee2f0276ac9b19fed2bc1941ed4c59ae5a68394
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-10-15 18:16:29 -0400

    new bfs

--- AI/edgesearch/edges.py
+++ AI/edgesearch/edges.py
@@ -1,37 +1,5 @@
 from collections import deque
 #ITS LIKE A REALLY AWESOME TO WAY TO DO THIS FAST.
-class BFS:
-    def __init__(self, node,edges, source):
-        self.node = node
-        self.edges = edges
-        self.source = source
-        self.queue = deque()
-        self.parent =[-1 for u in range(0,node)]
-        self.construct_graph()
-        self.bfs_traversal()
-
-    def construct_graph(self):
-        for u,v in self.edges:
-            self.graph[u][v], self.graph[v][u] = True, True
-
-    def bfs_traversal(self):
-        self.queue.append(self.source)
-        while len(self.queue):
-            u =  self.queue.popleft()
-            for v in range(0,self.node):
-                if self.graph[u][v] == True and self.color[v]=='W':
-                    self.queue.append(v)
-                    self.parent[v]=u
-
-    def print_shortest_path(self, destination):
-        if destination==self.source:
-            print destination,
-        elif self.parent[destination] == -1:
-            print "No Path"
-        else:
-            self.print_shortest_path(self.parent[destination])
-            print "-> ",destination,
-
 nodefile = open('nodes.txt','r')
 edgefile = open('edges.txt','r')
 nodelist = list(nodefile)
@@ -44,5 +12,7 @@ for i in range(len(nodelist)):
 for i in range(len(edgelist)):
     edgedict[edgelist[i].split(' ')[0]] = [edgelist[i].split(' ')[1],edgelist[i].split(' ')[2],edgelist[i].split(' ')[3].split('\n')[0]]
 
-print nodedict
-print edgedict
+edges = []
+for i in range(len(edgelist)):
+    edges.append((edgelist[i].split(' ')[1],edgelist[i].split(' ')[2]))
+
