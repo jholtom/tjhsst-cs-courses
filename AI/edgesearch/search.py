2e13f8839f2485bef773429d1fdeb3d2a545e6d4
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-10-23 19:38:34 -0400

    cleaning

--- AI/edgesearch/search.py
+++ AI/edgesearch/search.py
@@ -1,6 +1,7 @@
 #Uniform Cost and A-Star - Jacob Holtom - 10/23/13
 from pythonutils import *
 import math, random, sys, time, bisect, string
+
 def best_first_graph_search(problem, f):
     f = memoize(f, 'f')
     node = Node(problem.initial)
@@ -43,8 +44,3 @@ for i in range(len(nodelist)):
 for i in range(len(edgelist)):
     edgedict[edgelist[i].split(' ')[0]] = [edgelist[i].split(' ')[1],edgelist[i].split(' ')[2],edgelist[i].split(' ')[3].split('\n')[0]]
 
-graph = {}
-
-for i in 
-
-railroads = UndirectedGraph(graph)
