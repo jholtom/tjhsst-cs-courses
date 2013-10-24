c2e1ac2f014e28282c71ab871a60d27d55ab1d75
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-10-23 20:16:39 -0400

    stuff

--- AI/edgesearch/search.py
+++ AI/edgesearch/search.py
@@ -1,36 +1,36 @@
 #Uniform Cost and A-Star - Jacob Holtom - 10/23/13
-from pythonutils import *
-import math, random, sys, time, bisect, string
+import heapq
 
-def best_first_graph_search(problem, f):
-    f = memoize(f, 'f')
-    node = Node(problem.initial)
-    if problem.goal_test(node.state):
-        return node
-    frontier = PriorityQueue(min, f)
-    frontier.append(node)
-    explored = set()
-    while frontier:
-        node = frontier.pop()
-        if problem.goal_test(node.state):
-            return node
-        explored.add(node.state)
-        for child in node.expand(problem):
-            if child.state not in explored and child not in frontier:
-                frontier.append(child)
-            elif child in frontier:
-                incumbent = frontier[child]
-                if f(child) < f(incumbent):
-                    del frontier[incumbent]
-                    frontier.append(child)
-    return None
+def astar(self, graph, current, end, edges):
+    openSet = set()
+    openHeap = []
+    closedSet = set()
 
-def uniform_cost_search(problem):
-    return best_first_graph_search(problem, lambda node: node.path_cost)
+    def retracePath(c):
+        path = [c]
+        while c.parent is not None:
+            c = c.parent
+            path.append(c)
+        path.reverse()
+        return path
 
-def astar_search(problem, h=None):
-    h = memoize(h or problem.h, 'h')
-    return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
+    openSet.add(current)
+    openHeap.append((0,current))
+    while openSet:
+        current = heapq.heappop(openHeap)[1]
+        if current == end:
+            return retracePath(current)
+        openSet.remove(current)
+        closedSet.add(current)
+        for tile in graph[current]:
+            if tile not in closedSet:
+                #tile.H = (abs(end.x-tile.x)+abs(end.y-tile.y))*10 
+                tile.H = abs(edges[(tile,end)])
+                if tile not in openSet:
+                    openSet.add(tile)
+                    heapq.heappush(openHeap, (tile.H,tile))
+                tile.parent = current
+    return []
 
 nodefile = open('nodes.txt','r')
 edgefile = open('edges.txt','r')
@@ -44,3 +44,18 @@ for i in range(len(nodelist)):
 for i in range(len(edgelist)):
     edgedict[edgelist[i].split(' ')[0]] = [edgelist[i].split(' ')[1],edgelist[i].split(' ')[2],edgelist[i].split(' ')[3].split('\n')[0]]
 
+#ASSEMBLE GRAPH HERE
+graph = {}
+for i in nodedict:
+        graph[i] = []
+
+for i in nodedict:
+    for k in edgedict:
+        if i == edgedict[k][0]:
+            graph[i].append(edgedict[k][1]) 
+
+edges = {}
+for i in edgedict:
+    edges[(edgedict[i][0],edgedict[i][1])] = edgedict[i][2]
+
+print astar(graph,'1701291','0600209', edges)
