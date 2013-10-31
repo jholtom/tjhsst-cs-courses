2f523cb60e58cd7b56e733e9389734d1d4d8febf
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-10-30 22:56:51 -0400

    magic

--- AI/edgesearch/search.py
+++ AI/edgesearch/search.py
@@ -2,7 +2,43 @@
 import heapq
 import time
 import pickle
+
 def astar(graph, current, end, edges):
+    openSet = set()
+    openHeap = []
+    closedSet = set()
+    parents = {}
+    parents[current] = None
+    count = 0
+    def retracePath(c):
+        path = [c]
+        while parents[c] is not None:
+            c = parents[c]
+            path.append(c)
+        path.reverse()
+        return path
+
+    openSet.add(current)
+    openHeap.append((0,current))
+    while openSet:
+        current = heapq.heappop(openHeap)[1]
+        count += 1
+        if current == end:
+            return (retracePath(current),count)
+        openSet.remove(current)
+        closedSet.add(current)
+        #print 'Searching for paths from '+current
+        for loc in graph[current]:
+            if loc not in closedSet:
+                temp = abs(float(edges[frozenset([current,loc])]))
+                if loc not in openSet:
+                    openSet.add(loc)
+                    heapq.heappush(openHeap, (temp,loc))
+                parents[loc] = current
+                #print '\t'+loc
+    return ([],count)
+
+def ucs(graph, current, end, edges):
     openSet = set()
     openHeap = []
     closedSet = set()
@@ -39,15 +75,13 @@ def astar(graph, current, end, edges):
 
 graph = pickle.load(open('graph','r'))
 edges = pickle.load(open('edges','r'))
-print "RECOMMENDED: 1701291 to 0600209"
-print "RECOMMENDED: 2900190 to 2900275"
+#DAT TESTING METHOD
 while True:
     start = raw_input('Starting node?') 
     end = raw_input('Ending node?')
     start_time = time.time()
     path,count = astar(graph, start, end, edges)
+    #path,count = ucs(graph,start,end,edges)
     print time.time() - start_time, "seconds"
     print "Path Length: " + str(len(path))
-    print "Popped out of queue" + str(count)
-#for i in path:
-#    print i,' -> ',
+    print "Popped out of queue: " + str(count)
