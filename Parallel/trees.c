92be018cbc4561945ba82ffd4b5c13ad5271d923
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-10-30 23:16:20 -0400

    magic

--- Parallel/trees.c
+++ Parallel/trees.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "mpi.h"
 #define ARRX 100
 #define ARRY 100
@@ -110,19 +111,17 @@ int compute(char field[ARRX][ARRY], int prob){
 	}
 	return steps;
 }
-int selectnode(int);
-int selectnode(int np)
-{
-	return rand() % (np - 1) + 1;
-}
 void main( int argc , char* argv[] ){
 	int rank,size,prob,prob2;
 	MPI_Status status;
 	int tag = 0;
-	int step,j;
+    int count = 0;
+	int step,j,result;
+    int TRIALS_EACH = 10;
+	int numtrials = 1000;
+    int totals[numtrials/TRIALS_EACH];
 	char t[ARRX][ARRY];
 	int trial,iter;
-	int numtrials = 100;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
@@ -130,50 +129,40 @@ void main( int argc , char* argv[] ){
 	if( rank == 0 ) // manager
 	{
 
-		for (rank = 1; rank < ntasks; rank++){
+		for (rank = 1; rank < size; rank++){
 			prob = (int)floor(count % TRIALS_EACH);
-
-			MPI_Send(&prob, 1, MPI_INT, rank, WORKTAG, MPI_COMM_WORLD);
+			MPI_Send(&prob, 1, MPI_INT, rank, 0, MPI_COMM_WORLD);
 			count++;
 		}
 
-		while (count < TRIALS){
-			MPI_Recv(&result, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			MPI_Recv(&probab, 1, MPI_INT, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			totals[probab] += result;
+		while (count < numtrials){
+			MPI_Recv(&result, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			MPI_Recv(&prob2, 1, MPI_INT, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			totals[prob2] += result;
 			prob = floor(count / TRIALS_EACH);
-			MPI_Send(&prob, 1, MPI_INT, status.MPI_SOURCE, WORKTAG, MPI_COMM_WORLD);
+			MPI_Send(&prob, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
 			count++;
 		}
 
-		for (rank = 1; rank < ntasks; rank++){
-			MPI_Recv(&result, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			MPI_Recv(&probab, 1, MPI_INT, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			totals[probab] += result;
+		for (iter = 1; iter < size; iter++){
+			MPI_Recv(&result, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			MPI_Recv(&prob2, 1, MPI_INT, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			totals[prob2] += result;
 		}
 
-		for (rank = 1; rank < ntasks; rank++){
+		for (rank = 1; rank < size; rank++){
 			MPI_Send(0, 0, MPI_INT, rank, DIETAG, MPI_COMM_WORLD);
 		}
-
-		for (count = 0; count < (TRIALS/TRIALS_EACH); count++){
-			//printf("%.2f %.2f\n", (double)count/100, (double)totals[count]/100);
-		}
-
-		return 0;
 	}
 	else            // worker
 	{
-		MPI_Recv( &prob , 1 , MPI_DOUBLE , 0 , tag , MPI_COMM_WORLD , &status ) ;
+		MPI_Recv( &prob , 1 , MPI_INT , 0 , tag , MPI_COMM_WORLD , &status ) ;
 		if(status.MPI_TAG == DIETAG)
 			exit(0);
 		step = 0 ;
-		for( trial = 1 ; trial <= numtrials ; trial++ )
-		{
-			step += compute( t , prob ) ;
-		}
-		MPI_Send( &prob , 1 , MPI_INT , 0 , tag , MPI_COMM_WORLD );
-		MPI_Send( &step , 1 , MPI_INT    , 0 , tag , MPI_COMM_WORLD ) ;
+		step += compute( t , prob ) ;
+		MPI_Send( &step , 1 , MPI_INT , 0 , tag , MPI_COMM_WORLD );
+		MPI_Send( &prob , 1 , MPI_INT    , 0 , tag , MPI_COMM_WORLD ) ;
 	}
 	MPI_Finalize();
 }
