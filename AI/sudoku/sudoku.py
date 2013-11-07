aae0e525a9d7545c5bce46ec6d0ef087d9d9d693
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-11-07 13:34:40 -0500

    magic

--- AI/sudoku/sudoku.py
+++ AI/sudoku/sudoku.py
@@ -1,1 +1,4 @@
 #Jacob Holtom - 11/06/13
+puzzles = file("easy.txt").read().strip().split("\n")
+
+
--- Parallel/percolation/gl/gltrees.c
+++ Parallel/percolation/gl/gltrees.c
@@ -10,95 +10,6 @@
 #define ARRY N
 #define DIETAG 2
 #define WORKTAG 0
-void process_fire(char[ARRX][ARRY]);
-void process_fire(char field[ARRX][ARRY]){
-    char field_buffer[ARRX][ARRY];
-
-    int k,j;
-
-    for( k = 0; k < ARRX; k++)
-    {
-        for( j = 0; j < ARRY; j++)
-        {
-            field_buffer[k][j] = field[k][j];
-        }
-    }
-    for( k = 0; k < ARRX; k++)
-    {
-        for( j = 0; j < ARRY; j++)
-        {
-            if( field[k][j] == '*')
-            {
-                if(field[k-1][j] == 'T' && k-1 > 0){
-                    field_buffer[k-1][j] = '*';
-                }
-                if(field[k+1][j] == 'T' && k+1 < ARRX){
-                    field_buffer[k+1][j] = '*';
-                }
-                if(field[k][j-1] == 'T' && j-1 > 0){
-                    field_buffer[k][j-1] = '*';
-                }
-                if(field[k][j+1] == 'T' && j+1 < ARRY){
-                    field_buffer[k][j+1] = '*';
-                }
-                field_buffer[k][j] = ' ';
-            }
-        }
-    }
-
-    for( k = 0; k < ARRX; k++)
-    {
-        for( j = 0; j < ARRY; j++)
-        {
-            field[k][j] = field_buffer[k][j];
-        }
-    }
-}
-int isfire(char[ARRX][ARRY]);
-int isfire(char field[ARRX][ARRY])
-{
-    int l,z;
-    for(l = 0; l < ARRX; l++)
-    {
-        for(z = 0; z < ARRY; z++)
-        {
-            if(field[l][z] == '*')
-            {
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-int compute( char[ARRX][ARRY], int);
-int compute(char field[ARRX][ARRY], int prob){
-    int k;
-    int m;
-    for(k = 0; k < ARRX; k++){
-        for(m = 0; m < ARRY; m++)
-        {
-            if((rand()%100 + 1) < prob)
-            {
-                field[k][m] = 'T';
-            }
-            else{
-                field[k][m] = ' ';
-            }
-        }
-    }
-    int z;
-    for(z = 0; z < ARRX; z++)
-    {
-        if( field[z][0] == 'T' ) field[z][0] = '*';
-    }
-    int steps = 0;
-    while(isfire(field) != 0)
-    {
-        process_fire(field);
-        steps++;
-    }
-    return steps;
-}
 void idlefunc(void)
 {
 
@@ -165,40 +76,12 @@ int main(int argc,char* argv[])
 {  
     int rank,size,prob,prob2;
     MPI_Status status;
-    int count = 0;
-    int step,j,result;
-    int TRIALS_EACH = 100;
-    int numtrials = 10000;
-    int totals[numtrials/TRIALS_EACH];
-    char t[ARRX][ARRY];
-    int trial,iter;
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     srand(time(NULL));
     if( rank == 0 ) // manager  - I AM VERY GL
     {
-        //seed them
-        for (iter = 1; iter < size; iter++){
-            prob = (int)floor(count % TRIALS_EACH);
-            MPI_Send(&prob, 1, MPI_INT, iter, WORKTAG, MPI_COMM_WORLD);
-            count++;
-        }
-        //feed them
-        while (count < numtrials){
-            MPI_Recv(&result, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            MPI_Recv(&prob2, 1, MPI_INT, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            totals[prob2] += result;
-            prob = floor(count / TRIALS_EACH);
-            MPI_Send(&prob, 1, MPI_INT, status.MPI_SOURCE, WORKTAG, MPI_COMM_WORLD);
-            count++;
-        }
-        //get stuff
-        for (iter = 1; iter < size; iter++){
-            MPI_Recv(&result, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            MPI_Recv(&prob2, 1, MPI_INT, status.MPI_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            totals[prob2] += result;
-        }
         glutInit(&argc,argv);
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
         glutInitWindowSize(N,N);
