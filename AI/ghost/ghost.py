c53f62864ffcee1ee70bb02d009af5706046d89b
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-12-05 09:10:29 -0500

    magic and stuff

--- AI/ghost/ghost.py
+++ AI/ghost/ghost.py
@@ -1,3 +1,16 @@
 #Jacob Holtom - G.H.O.S.T 12/02/13
 dictionary = file("dictionary.txt").read().split("\n")
-print dictionary
+def isWord(a):
+    if a in dictionary:
+        return False
+    return True
+print "Welcome to G.H.O.S.T"
+a = raw_input("Enter a letter: ")
+turn0 = 0
+turn1 = 0
+while not isWord(a):
+    if turn0 == 1:
+        a = raw_input("Enter a letter: ")
+    if turn1 == 1:
+        a = raw_input("Enter a letter: ")
+
