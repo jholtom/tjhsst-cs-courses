4225a986418f7782c72178c8aa281df847357880
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-12-11 19:46:24 -0500

    fixed stuff and count

--- AI/sudoku/sudoku.py
+++ AI/sudoku/sudoku.py
@@ -5,14 +5,16 @@ def ncol(i,j): return (i-j) % 9 == 0
 def nblock(i,j): return (i/27 == j/27 and i%9/3 == j%9/3)
 
 def r(a):
+  global count
   i = a.find('.')
   if i == -1:
-    print "solved"
+    print "solved with backtrack count: " + str(count)
     sys.exit()
   
   ignores = set()
   for j in range(81):
     if nrow(i,j) or ncol(i,j) or nblock(i,j):
+      count += 1
       ignores.add(a[j])
 
   for m in '123456789':
@@ -23,4 +25,5 @@ filename = raw_input('What file to solve? ')
 puzzles = file(filename).read().strip().split("\n")
 for i in puzzles:
   print "Solving: "+i
+  count = 0
   r(i)
