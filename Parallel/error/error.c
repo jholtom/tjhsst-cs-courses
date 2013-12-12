967aa25201d66dd329e280d63796c00c1ad18116
Jacob Holtom <webmasterlegobrains@gmail.com>
2013-12-12 13:24:17 -0500

    makefile and error code

---
+++ Parallel/error/error.c
@@ -0,0 +1,15 @@
+//Jacob Holtom - Error and whatnot
+//12/12/13
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+double sm( double y ) {
+
+}
+int bit( double x ) {
+
+}
+int main(void){
+return 0;
+}
