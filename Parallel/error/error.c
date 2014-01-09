154dbe6079703b9b6f4ef490ea300026003f465c
Jacob Holtom <webmasterlegobrains@gmail.com>
2014-01-08 22:09:13 -0500

    clean

--- Parallel/error/error.c
+++ Parallel/error/error.c
@@ -21,96 +21,96 @@ int main(int argc,char* argv[]){
     srand(time(NULL));
     //Local Variables
     int    i, j, k, p, np, op, ranpat[NUMPAT+1], epoch;
-    int    NumPattern = NUMPAT, NumInput = NUMIN, NumHidden = NUMHID, NumOutput = NUMOUT;
-    double Input[NUMPAT+1][NUMIN+1] = { 0, 0, 0,  0, 0, 0,  0, 1, 0,  0, 0, 1,  0, 1, 1 };
-    double Target[NUMPAT+1][NUMOUT+1] = { 0, 0,  0, 0,  0, 1,  0, 1,  0, 0 };
-    double SumH[NUMPAT+1][NUMHID+1], WeightIH[NUMIN+1][NUMHID+1], Hidden[NUMPAT+1][NUMHID+1];
-    double SumO[NUMPAT+1][NUMOUT+1], WeightHO[NUMHID+1][NUMOUT+1], Output[NUMPAT+1][NUMOUT+1];
-    double DeltaO[NUMOUT+1], SumDOW[NUMHID+1], DeltaH[NUMHID+1];
-    double DeltaWeightIH[NUMIN+1][NUMHID+1], DeltaWeightHO[NUMHID+1][NUMOUT+1];
+    int    npa = NUMPAT, Numin = NUMIN, Numh = NUMHID, Numout = NUMOUT;
+    double in[NUMPAT+1][NUMIN+1] = { 0, 0, 0,  0, 0, 0,  0, 1, 0,  0, 0, 1,  0, 1, 1 };
+    double targ[NUMPAT+1][NUMOUT+1] = { 0, 0,  0, 0,  0, 1,  0, 1,  0, 0 };
+    double sh[NUMPAT+1][NUMHID+1], wih[NUMIN+1][NUMHID+1], h[NUMPAT+1][NUMHID+1];
+    double so[NUMPAT+1][NUMOUT+1], who[NUMHID+1][NUMOUT+1], out[NUMPAT+1][NUMOUT+1];
+    double deo[NUMOUT+1], sdow[NUMHID+1], dh[NUMHID+1];
+    double dwih[NUMIN+1][NUMHID+1], dwho[NUMHID+1][NUMOUT+1];
     double Error, eta = 0.5, alpha = 0.9, smallwt = 0.5;
     //Initializer loops
-    for( j = 1 ; j <= NumHidden ; j++ ) {    /* initialize WeightIH and DeltaWeightIH */
-        for( i = 0 ; i <= NumInput ; i++ ) { 
-            DeltaWeightIH[i][j] = 0.0 ;
-            WeightIH[i][j] = 2.0 * ( bit() - 0.5 ) * smallwt ;
+    for( j = 1 ; j <= Numh ; j++ ) {
+        for( i = 0 ; i <= Numin ; i++ ) { 
+            dwih[i][j] = 0.0 ;
+            wih[i][j] = 2.0 * ( bit() - 0.5 ) * smallwt ;
         }
     }
-    for( k = 1 ; k <= NumOutput ; k ++ ) {    /* initialize WeightHO and DeltaWeightHO */
-        for( j = 0 ; j <= NumHidden ; j++ ) {
-            DeltaWeightHO[j][k] = 0.0 ;              
-            WeightHO[j][k] = 2.0 * ( bit() - 0.5 ) * smallwt ;
+    for( k = 1 ; k <= Numout ; k ++ ) {
+        for( j = 0 ; j <= Numh ; j++ ) {
+            dwho[j][k] = 0.0 ;              
+            who[j][k] = 2.0 * ( bit() - 0.5 ) * smallwt ;
         }
     }
     //Main loop
-    for( epoch = 0 ; epoch < 100000 ; epoch++) {    /* iterate weight updates */
-        for( p = 1 ; p <= NumPattern ; p++ ) {    /* bitmize order of individuals */
+    for( epoch = 0 ; epoch < 100000 ; epoch++) {
+        for( p = 1 ; p <= npa ; p++ ) {    
             ranpat[p] = p ;
         }
-        for( p = 1 ; p <= NumPattern ; p++) {
-            np = p + bit() * ( NumPattern + 1 - p ) ;
+        for( p = 1 ; p <= npa ; p++) {
+            np = p + bit() * ( npa + 1 - p ) ;
             op = ranpat[p] ; ranpat[p] = ranpat[np] ; ranpat[np] = op ;
         }
         Error = 0.0 ;
-        for( np = 1 ; np <= NumPattern ; np++ ) {    /* repeat for all the training patterns */
+        for( np = 1 ; np <= npa ; np++ ) {
             p = ranpat[np];
-            for( j = 1 ; j <= NumHidden ; j++ ) {    /* compute hidden unit activations */
-                SumH[p][j] = WeightIH[0][j] ;
-                for( i = 1 ; i <= NumInput ; i++ ) {
-                    SumH[p][j] += Input[p][i] * WeightIH[i][j] ;
+            for( j = 1 ; j <= Numh ; j++ ) {
+                sh[p][j] = wih[0][j] ;
+                for( i = 1 ; i <= Numin ; i++ ) {
+                    sh[p][j] += in[p][i] * wih[i][j] ;
                 }
-                Hidden[p][j] = sm(SumH[p][j]) ;
+                h[p][j] = sm(sh[p][j]) ;
             }
-            for( k = 1 ; k <= NumOutput ; k++ ) {    /* compute output unit activations and errors */
-                SumO[p][k] = WeightHO[0][k] ;
-                for( j = 1 ; j <= NumHidden ; j++ ) {
-                    SumO[p][k] += Hidden[p][j] * WeightHO[j][k] ;
+            for( k = 1 ; k <= Numout ; k++ ) {
+                so[p][k] = who[0][k] ;
+                for( j = 1 ; j <= Numh ; j++ ) {
+                    so[p][k] += h[p][j] * who[j][k] ;
                 }
-                Output[p][k] = sm(SumO[p][k]) ;   /* Sigmoidal Outputs */
-                Error += 0.5 * (Target[p][k] - Output[p][k]) * (Target[p][k] - Output[p][k]) ;   /* SSE */
-                DeltaO[k] = (Target[p][k] - Output[p][k]) * Output[p][k] * (1.0 - Output[p][k]) ;   /* Sigmoidal Outputs, SSE */
+                out[p][k] = sm(so[p][k]) ; 
+                Error += 0.5 * (targ[p][k] - out[p][k]) * (targ[p][k] - out[p][k]) ;
+                deo[k] = (targ[p][k] - out[p][k]) * out[p][k] * (1.0 - out[p][k]) ;
             }
-            for( j = 1 ; j <= NumHidden ; j++ ) {    /* 'back-propagate' errors to hidden layer */
-                SumDOW[j] = 0.0 ;
-                for( k = 1 ; k <= NumOutput ; k++ ) {
-                    SumDOW[j] += WeightHO[j][k] * DeltaO[k] ;
+            for( j = 1 ; j <= Numh ; j++ ) {    
+                sdow[j] = 0.0 ;
+                for( k = 1 ; k <= Numout ; k++ ) {
+                    sdow[j] += who[j][k] * deo[k] ;
                 }
-                DeltaH[j] = SumDOW[j] * Hidden[p][j] * (1.0 - Hidden[p][j]) ;
+                dh[j] = sdow[j] * h[p][j] * (1.0 - h[p][j]) ;
             }
-            for( j = 1 ; j <= NumHidden ; j++ ) {     /* update weights WeightIH */
-                DeltaWeightIH[0][j] = eta * DeltaH[j] + alpha * DeltaWeightIH[0][j] ;
-                WeightIH[0][j] += DeltaWeightIH[0][j] ;
-                for( i = 1 ; i <= NumInput ; i++ ) { 
-                    DeltaWeightIH[i][j] = eta * Input[p][i] * DeltaH[j] + alpha * DeltaWeightIH[i][j];
-                    WeightIH[i][j] += DeltaWeightIH[i][j] ;
+            for( j = 1 ; j <= Numh ; j++ ) {
+                dwih[0][j] = eta * dh[j] + alpha * dwih[0][j] ;
+                wih[0][j] += dwih[0][j] ;
+                for( i = 1 ; i <= Numin ; i++ ) { 
+                    dwih[i][j] = eta * in[p][i] * dh[j] + alpha * dwih[i][j];
+                    wih[i][j] += dwih[i][j] ;
                 }
             }
-            for( k = 1 ; k <= NumOutput ; k ++ ) {    /* update weights WeightHO */
-                DeltaWeightHO[0][k] = eta * DeltaO[k] + alpha * DeltaWeightHO[0][k] ;
-                WeightHO[0][k] += DeltaWeightHO[0][k] ;
-                for( j = 1 ; j <= NumHidden ; j++ ) {
-                    DeltaWeightHO[j][k] = eta * Hidden[p][j] * DeltaO[k] + alpha * DeltaWeightHO[j][k] ;
-                    WeightHO[j][k] += DeltaWeightHO[j][k] ;
+            for( k = 1 ; k <= Numout ; k ++ ) {
+                dwho[0][k] = eta * deo[k] + alpha * dwho[0][k] ;
+                who[0][k] += dwho[0][k] ;
+                for( j = 1 ; j <= Numh ; j++ ) {
+                    dwho[j][k] = eta * h[p][j] * deo[k] + alpha * dwho[j][k] ;
+                    who[j][k] += dwho[j][k] ;
                 }
             }
         }
         if( epoch%100 == 0 ) printf("\nEpoch %-5d :   Error = %f", epoch, Error) ;
-        if( Error < 0.0004 ) break ;  /* stop learning when 'near enough' */
+        if( Error < 0.0004 ) break ;
     }
-    printf("\n\nNETWORK DATA - EPOCH %d\n\nPat\t", epoch) ;   /* print network outputs */
-    for( i = 1 ; i <= NumInput ; i++ ) {
-        printf("Input%-4d\t", i) ;
+    printf("\n\nNETWORK DATA - EPOCH %d\n\nPat\t", epoch) ;
+    for( i = 1 ; i <= Numin ; i++ ) {
+        printf("in%-4d\t", i) ;
     }
-    for( k = 1 ; k <= NumOutput ; k++ ) {
-        printf("Target%-4d\tOutput%-4d\t", k, k) ;
+    for( k = 1 ; k <= Numout ; k++ ) {
+        printf("targ%-4d\tout%-4d\t", k, k) ;
     }
-    for( p = 1 ; p <= NumPattern ; p++ ) {        
+    for( p = 1 ; p <= npa ; p++ ) {        
     printf("\n%d\t", p) ;
-        for( i = 1 ; i <= NumInput ; i++ ) {
-            printf("%f\t", Input[p][i]) ;
+        for( i = 1 ; i <= Numin ; i++ ) {
+            printf("%f\t", in[p][i]) ;
         }
-        for( k = 1 ; k <= NumOutput ; k++ ) {
-            printf("%f\t%f\t", Target[p][k], Output[p][k]) ;
+        for( k = 1 ; k <= Numout ; k++ ) {
+            printf("%f\t%f\t", targ[p][k], out[p][k]) ;
         }
     }
     return 1 ;
