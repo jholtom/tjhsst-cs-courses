0c14f51131b3f36aa325175d117faca45efae461
Jacob Holtom <webmasterlegobrains@gmail.com>
2014-02-04 10:35:25 -0500

    asdf

--- AI/imageprocessing/ppm.py
+++ AI/imageprocessing/ppm.py
@@ -2,14 +2,12 @@
 data = open('image.ppm').read().split()
 w = int(data[1])
 h = int(data[2])
-print w
-print h
 data = data[4:]
-
 y = 0
 while y < h:
     x = 0
     while x < w:
         j = 3 * (y * w + x)
+        print str(int((int(data[j]) * 0.30))) + ' ' +  str(int((int(data[j+1]) * 0.59))) + ' ' + str(int((int(data[j+2]) * 0.11)))
         x += 1
     y += 1
