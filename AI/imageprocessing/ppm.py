e3c11d1a5e331452e1a3fb969895d1a6cef98415
Jacob Holtom <webmasterlegobrains@gmail.com>
2014-02-05 20:34:39 -0500

    clean

--- AI/imageprocessing/ppm.py
+++ AI/imageprocessing/ppm.py
@@ -1,4 +1,5 @@
 # Jacob Holtom
+import math
 data = open('image.ppm').read().split()
 w = int(data[1])
 h = int(data[2])
@@ -8,6 +9,14 @@ while y < h:
     x = 0
     while x < w:
         j = 3 * (y * w + x)
-        print str(int((int(data[j]) * 0.30))) + ' ' +  str(int((int(data[j+1]) * 0.59))) + ' ' + str(int((int(data[j+2]) * 0.11)))
+        #print str(int((int(data[j]) * 0.30))) + ' ' +  str(int((int(data[j+1]) * 0.59))) + ' ' + str(int((int(data[j+2]) * 0.11)))
+        r = data[j]
+        g = data[j+1]
+        b = data[j+2]
         x += 1
     y += 1
+
+def blur(x,y,omega):
+    """Applies Gaussian Function to x and y based on omega"""
+    g = (1 / (2*math.pi*(omega ** 2))) * math.e ** ( -1 * (((x ** 2) + (y ** 2)) / ( 2 * (omega ** 2))))
+    return g
--- Parallel/raytracing/tracer.c
+++ Parallel/raytracing/tracer.c
@@ -3,31 +3,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define WIDTH 640
-#define HIGHT 480
-void write(int rgb[HIGHT][WIDTH][3])
+#define W 640
+#define H 480
+#define for_x for (int x = 0; x < W; x++)
+#define for_y for (int y = 0; y < H; y++)
+#define for_xy for_x for_y
+int rgb[H][W][3];
+void write(int rgb[H][W][3])
 {
    int y , x ;
    FILE* fout ;
    fout = fopen( "output.ppm" , "w" ) ;
    fprintf( fout , "P3\n" ) ;
-   fprintf( fout , "%d %d\n" , WIDTH , HIGHT ) ;
+   fprintf( fout , "%d %d\n" , W , H ) ;
    fprintf( fout , "255\n" ) ;
-   for( y = 0 ; y < HIGHT ; y++ )
+   for( y = 0 ; y < H ; y++ )
    {
-      for( x = 0 ; x < WIDTH ; x++)
+      for( x = 0 ; x < W ; x++)
       {
          fprintf( fout , "%d %d %d\n" , rgb[y][x][0] , rgb[y][x][1] , rgb[y][x][2] ) ;
       }
    }
    close( fout ) ;
 }
+void tracer(int rgb[H][W][3]){
+
+}
 int main(void){
    int y, x;
-   int rgb[HIGHT][WIDTH][3];
-    for( y = 0 ; y < HIGHT ; y++ )
+    for( y = 0 ; y < H ; y++ )
    {
-      for( x = 0 ; x < WIDTH ; x++)
+      for( x = 0 ; x < W ; x++)
       {
          rgb[y][x][0] = 0   ; // red
          rgb[y][x][1] = 255 ; // green
