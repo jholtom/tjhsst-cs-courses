e6c0a4ea724ebd58613c052e0e3d94109436f9bb
Jacob Holtom <webmasterlegobrains@gmail.com>
2014-02-10 21:16:25 -0500

    asdf

--- AI/imageprocessing/ppm.py
+++ AI/imageprocessing/ppm.py
@@ -1,5 +1,6 @@
 # Jacob Holtom
 import math
+import numpy as np
 data = open('image.ppm').read().split()
 w = int(data[1])
 h = int(data[2])
@@ -9,16 +10,21 @@ while y < h:
     x = 0
     while x < w:
         j = 3 * (y * w + x)
-        #print str(int((int(data[j]) * 0.30))) + ' ' +  str(int((int(data[j+1]) * 0.59))) + ' ' + str(int((int(data[j+2]) * 0.11)))
         m = int((int(data[j]) * 0.30)) + int((int(data[j+1]) * 0.59)) + int((int(data[j+2]) * 0.11))
-        print m + ' ' + m + ' ' + m
+        print str(m) + ' ' + str(m) + ' ' + str(m)
         #r = data[j]
         #g = data[j+1]
         #b = data[j+2]
         x += 1
     y += 1
 
-def blur(x,y,omega):
-    """Applies Gaussian Function to x and y based on omega"""
-    g = (1 / (2*math.pi*(omega ** 2))) * math.e ** ( -1 * (((x ** 2) + (y ** 2)) / ( 2 * (omega ** 2))))
-    return g
+def gauss_kern():
+   """ Returns a normalized 2D gauss kernel array for convolutions """
+   h1 = w
+   h2 = h
+   x, y = np.mgrid[0:h2, 0:h1]
+   x = x-h2/2
+   y = y-h1/2
+   sigma = 1.5
+   g = np.exp( -( x**2 + y**2 ) / (2*sigma**2) );
+   return g / g.sum()
