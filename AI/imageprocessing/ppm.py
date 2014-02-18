6983a0ad9f31f44519e1128b67afdef08c5c9863
Jacob Holtom <webmasterlegobrains@gmail.com>
2014-02-17 20:01:17 -0500

    grayscale and gauss

--- AI/imageprocessing/ppm.py
+++ AI/imageprocessing/ppm.py
@@ -1,6 +1,18 @@
 # Jacob Holtom
 import math
 import numpy as np
+
+def gauss_kern():
+   """ Returns a normalized 2D gauss kernel array for convolutions """
+   h1 = w
+   h2 = h
+   x, y = np.mgrid[0:h2, 0:h1]
+   x = x-h2/2
+   y = y-h1/2
+   sigma = 1.5
+   g = np.exp( -( x**2 + y**2 ) / (2*sigma**2) );
+   return g / g.sum()
+
 data = open('image.ppm').read().split()
 w = int(data[1])
 h = int(data[2])
@@ -13,17 +25,8 @@ while y < h:
         j = 3 * (y * w + x)
         m = int((int(data[j]) * 0.30)) + int((int(data[j+1]) * 0.59)) + int((int(data[j+2]) * 0.11))
         array[x][y] = m
-        print str(array[x][y]) + ' ' + str(array[x][y]) + ' ' + str(array[x][y])
+#        print str(array[x][y]) + ' ' + str(array[x][y]) + ' ' + str(array[x][y])
         x += 1
     y += 1
 
-def gauss_kern():
-   """ Returns a normalized 2D gauss kernel array for convolutions """
-   h1 = w
-   h2 = h
-   x, y = np.mgrid[0:h2, 0:h1]
-   x = x-h2/2
-   y = y-h1/2
-   sigma = 1.5
-   g = np.exp( -( x**2 + y**2 ) / (2*sigma**2) );
-   return g / g.sum()
+gauss = gauss_kern()
