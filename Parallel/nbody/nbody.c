1cb5d9e12ef8420cb92b3b10b5b6de3ab9764d45
Jacob Holtom <webmasterlegobrains@gmail.com>
2014-04-21 18:43:35 -0400

    cleaning and nbody

--- Parallel/nbody/nbody.c
+++ Parallel/nbody/nbody.c
@@ -1,13 +1,8 @@
-// 
-// Torbert, April 2014
-// 
 #include <stdio.h>
 #include <math.h>
 #include <GL/glut.h>
-//
 #define N  2
 #define DT 0.00001
-//
 typedef struct
 {
    double vx , vy , vz ;
@@ -15,13 +10,9 @@ typedef struct
    double r ;
    double m ;
 } Body;
-//
 Body nbody[N];
-//
-//
 double rho,phi,theta,up=1.0;
 double xc,yc,zc,xe,ye,ze;
-//
 void look()
 {
    xe=xc+rho*sin(theta)*cos(phi);
@@ -34,7 +25,6 @@ void look()
 void display(void)
 {
    int j;
-   //
    glMatrixMode(GL_MODELVIEW);
    glClear(GL_COLOR_BUFFER_BIT);
    glColor3f(0.0,0.0,0.0);
@@ -53,10 +43,8 @@ void idle(void)
    int j,k;
    double dx,dy,dz,dmag,fmag;
    double ax[N],ay[N],az[N];
-   //
    // vx += ax * DT
    //  x += vx * DT
-   //
    look();
    glutPostRedisplay();
 }
@@ -93,11 +81,9 @@ void reshape(int wscr,int hscr)
    GLfloat aspect_ratio;
    aspect_ratio=(GLfloat)wscr/(GLfloat)hscr;
    glViewport(0,0,(GLsizei)wscr,(GLsizei)hscr);
-   //
    glMatrixMode(GL_PROJECTION);
    glLoadIdentity();
    gluPerspective(60.0,aspect_ratio,0.1,500.0);
-   //
    look();
 }
 int main(int argc,char* argv[])
@@ -106,7 +92,6 @@ int main(int argc,char* argv[])
    phi=0.5*M_PI;
    theta=0.5*M_PI;
    xc=yc=zc=0.0;
-   //
    nbody[0].m=10000.0; // sun
    nbody[0].r=0.2;
    nbody[0].x=0.0;
@@ -115,7 +100,6 @@ int main(int argc,char* argv[])
    nbody[0].vx=0.0;
    nbody[0].vy=0.0;
    nbody[0].vz=0.0;
-   //
    nbody[1].m=100.0; // earth
    nbody[1].r=0.01;
    nbody[1].x=0.45;
@@ -124,16 +108,13 @@ int main(int argc,char* argv[])
    nbody[1].vx=0.0;
    nbody[1].vy=0.0;
    nbody[1].vz=0.0;
-   //
    glutInit(&argc,argv);
    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
    glutInitWindowSize(640,480);
    glutInitWindowPosition(100,50);
    glutCreateWindow("Sun Earth Moon");
-   //
    glClearColor(1.0,1.0,1.0,0.0);
    glShadeModel(GL_SMOOTH);
-   //
    glutDisplayFunc(display);
    glutIdleFunc(idle);
    glutMouseFunc(mouse);
@@ -141,11 +122,6 @@ int main(int argc,char* argv[])
    glutMouseWheelFunc(mouse_wheel);
    glutKeyboardFunc(keyfunc);
    glutReshapeFunc(reshape);
-   //
    glutMainLoop();
-   //
    return 0;
 }
-//
-// end of file
-//
